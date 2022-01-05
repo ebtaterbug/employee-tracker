5417a21494763fada2553ac45bc5f640011b953a
ebtaterbug <ityjoseph@gmail.com>
2022-01-05 09:43:38 -0800

    create files & folders

---
+++ db/connection.js
@@ -0,0 +1,17 @@
+const mysql = require('mysql2');
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+
+        user: process.env.DB_USER,
+
+        password: process.env.DB_PW,
+        
+        database: process.env.DB_NAME
+      },
+      console.log('Connected to database.')
+);
+
+module.exports = db;
---
+++ index.js
@@ -0,0 +1,4 @@
+const cTable = require('console.table');
+const mysql = require('mysql2');
+var inquirer = require('inquirer');
+
